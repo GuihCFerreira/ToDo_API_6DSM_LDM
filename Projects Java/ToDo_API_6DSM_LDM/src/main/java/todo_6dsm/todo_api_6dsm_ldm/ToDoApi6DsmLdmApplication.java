0c714f765a133d049559bd8cf1f8ec5a970939f8
GuihCFerreira <guihhh231@gmail.com>
2025-04-10 22:16:22 -0300

    initial commit

---
+++ Projects Java/ToDo_API_6DSM_LDM/src/main/java/todo_6dsm/todo_api_6dsm_ldm/ToDoApi6DsmLdmApplication.java
@@ -0,0 +1,13 @@
+package todo_6dsm.todo_api_6dsm_ldm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApi6DsmLdmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoApi6DsmLdmApplication.class, args);
+    }
+
+}
---
+++ Projects Java/ToDo_API_6DSM_LDM/src/test/java/todo_6dsm/todo_api_6dsm_ldm/ToDoApi6DsmLdmApplicationTests.java
@@ -0,0 +1,13 @@
+package todo_6dsm.todo_api_6dsm_ldm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApi6DsmLdmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
